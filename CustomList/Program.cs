115addf98916ab8df2d62df7e606a3a8a9ab80c7
estbarajas <ebarajas@wolfmail.stritch.edu>
2018-07-03 14:37:28 -0500

    Setup the projects and classes needed for this assignment.

--- CustomList/Program.cs
+++ CustomList/Program.cs
@@ -10,6 +10,8 @@ namespace CustomList
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World"); ;
+            Console.ReadLine();
         }
     }
 }

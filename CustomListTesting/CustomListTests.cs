2a77db3b46804388eb8d4a3428a4936e71f06d94
estbarajas <ebarajas@wolfmail.stritch.edu>
2018-07-03 14:33:52 -0500

    Setup the projects and classes needed/.

---
+++ CustomListTesting/CustomListTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CustomListTesting
+{
+    [TestClass]
+    public class CustomListTests //MethodName_StateUnderTest_ExpectedBehavior
+    {
+        [TestMethod]
+        public void Add_IndexZeroWithOneValue_ReturnNumber()
+        {
+            //Arrange
+            CustomList<int> theList = new CustomList<int>();
+            int expected = 100;
+
+            //Act
+            theList.Add(expected);
+
+            //Assert
+            Assert.AreEqual(expected, theList[0]);
+        }
+
+        [TestMethod]
+        public void Add_IndexZeroWithTwoValues_ReturnNumber()
+        {
+            //Arrange
+            CustomList<int> theList = new CustomList<int>();
+            int expected = 100;
+            int value = 200;
+
+            //Act
+            theList.Add(expected);
+            theList.Add(value);
+
+            //Assert
+            Assert.AreEqual(expected, theList[0]);
+        }
+
+        [TestMethod]
+        public void Add_IndexZeroWithMultipleValues_ReturnNumber()
+        {
+            //Arrange
+            CustomList<int> theList = new CustomList<int>();
+            int expected = 100;
+            int value = 200;
+
+            //Act
+            theList.Add(expected);
+
+            for (int i = 0; i < 15; i++)
+            {
+                theList.Add(value);
+                value += 100;
+            }
+
+            //Assert
+            Assert.AreEqual(expected, theList[0]);
+        }
+    }
+}
---
+++ CustomListTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CustomListTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CustomListTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6ceb62f8-0194-4d58-b29d-bfcd0e5f98bc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

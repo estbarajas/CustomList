8a324f9312be84dd73e3474d6258ba8e4cfd07c7
estbarajas <ebarajas@wolfmail.stritch.edu>
2018-07-05 07:19:46 -0500

    Add and Remove methods have 3 to 4 tests so far.

--- CustomListTesting/CustomListTests.cs
+++ CustomListTesting/CustomListTests.cs
@@ -56,5 +56,69 @@ namespace CustomListTesting
             //Assert
             Assert.AreEqual(expected, theList[0]);
         }
+
+        [TestMethod]
+        public void Add_PreExistingIndexZero_ReturnNumber()
+        {
+            //Arrange
+            CustomList<int> theList = new CustomList<int>(){10};
+            int expected = 20;
+            int value = 20;
+
+            //Act
+            theList[0].Add(value);
+
+            //Assert
+            Assert.AreEqual(expected, theList[0]);
+        }
+
+        public void Remove_IndexZeroHoldingOneValue_ReturnNull()
+        {
+            //Arrange
+            CustomList<int> theList = new CustomList<int>(){10};
+            string expected = null;
+            string actual;
+
+            //Act
+            theList.Remove.ElementAt(0);
+            actual = theList[0];
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void Remove_IndexZeroHoldingTwoValues_ReturnNull()
+        {
+            //Arrange
+            CustomList<int> theList = new CustomList<int>() {10,20};
+            int expected = 20;
+            int actual;
+
+            //Act
+            theList.Remove.ElementAt(0);
+            actual = theList[0];
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void Remove_IndexZeroHoldingMultipleValues_ReturnNumber()
+        {
+            //Arrange
+            CustomList<int> theList = new CustomList<int>() {10,20,30,40,50,60,70,80,90,100,110,120};
+            int expected = 20;
+            int actual;
+
+            //Act
+            theList.Remove.ElementAt(0);
+            theList.Remove.ElementAt(5);
+            theList.Remove.ElementAt(6);
+            theList.Remove.ElementAt(2);
+            actual = theList[0];
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
     }
 }
